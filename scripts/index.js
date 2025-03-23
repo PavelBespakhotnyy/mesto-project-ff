23c658f39b8743d4ba211cd7e8df58292fa48da3
Pavel Bespakhotnyi <pavel.bespakhotnyy@gmail.com>
2025-03-23 15:35:27 +0100

    First commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(item, removeCard) {
+  var cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  var cardImage = cardElement.querySelector('.card__image');
+  var cardTitle = cardElement.querySelector('.card__title');
+  var deleteButton = cardElement.querySelector('.card__delete-button');
 
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
+
+  deleteButton.addEventListener('click', function() {
+		removeCard(cardElement);
+  });
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
+function removeCard(cardElement) {
+	if (cardElement) {
+		cardElement.remove();
+	}
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => {
+	const cardElement = createCard(item, removeCard);
+	placesList.append(cardElement);
+});
